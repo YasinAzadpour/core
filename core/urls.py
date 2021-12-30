d1f553538d2feef119db7128a06c03d82d4fa71c
Yasin Azadpour <yasinaazadpour@gmail.com>
2021-12-30 16:12:22 +0330

    some config about media,static...

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.urls.conf import include
 
@@ -21,4 +24,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    *staticfiles_urlpatterns(),
+    *static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT),
 ]
