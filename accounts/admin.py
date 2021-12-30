36e0e396ff823a7821750446d7e8fbce62e2fc7e
Yasin Azadpour <yasinaazadpour@gmail.com>
2021-12-30 16:08:05 +0330

    custom user model added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,2 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('profile','header','name', 'category', 'bio', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    list_display = ('username', 'email', 'name', 'category', 'is_staff','is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'name', 'category', 'email')
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class MyUsernameValidator(validators.RegexValidator):
+    regex = r'^(?!.*\.\.)(?!.*\.$)[^\W][a-z0-9_.]{2,29}$'
+    message = _('Enter a valid username!')
+    flags = 0
+
+
+class User(AbstractUser):
+
+    username_validator = MyUsernameValidator()
+
+    username = models.CharField(unique=True,max_length=30,validators=[username_validator])
+    name = models.CharField(blank=True,max_length=50)
+    category = models.CharField(blank=True,max_length=30)
+    bio = models.TextField(blank=True,max_length=150,default="Hi there!")
+    email = models.EmailField(unique=True)
+    header = models.ImageField(blank=True)
+    profile = models.ImageField(blank=True)
+    first_name = last_name = None  # use name instead of first_name and last_name
 
