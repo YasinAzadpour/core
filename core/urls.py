64d7847533c4589853c811ec685ae69f0590054e
Yasin Azadpour <yasinaazadpour@gmail.com>
2022-01-04 12:50:21 +0330

    api app added

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    path('api/',include('api.urls')),
     *staticfiles_urlpatterns(),
     *static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT),
 ]
